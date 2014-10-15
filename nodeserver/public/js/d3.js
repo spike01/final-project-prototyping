58c7dcb8f28ba7076087cdf4288c0bf20ab771a1
Spike Nox <spike01@gmail.com>
2014-10-15 21:18:59 +0100

    Extracts d3 to its own file

---
+++ nodeserver/public/js/d3.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  var socket = io.connect('http://localhost');
+  var data = []
+
+  var width = 3600,
+height = 1800;
+
+var x = d3.scale.linear()
+  .domain([0, width])
+  .range([0, width]);
+
+var y = d3.scale.linear()
+  .domain([0, height])
+  .range([height, 0]);
+
+var canvas = d3.select("#container").append("canvas")
+  .attr("width", width)
+  .attr("height", height)
+  .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", zoom))
+  .node().getContext("2d");
+
+draw();
+
+function zoom() {
+  canvas.clearRect(0, 0, width, height);
+  draw();
+}
+
+function draw() {
+  var i = -1, n = data.length, d, cx, cy;
+  canvas.beginPath();
+  while (++i < n) {
+    d = data[i];
+    cx = x(d[0]);
+    cy = y(d[1]);
+    canvas.moveTo(cx, cy);
+    canvas.arc(cx, cy, 2.5, 0, 2 * Math.PI);
+    canvas.fillStyle = "white"
+  }
+  canvas.fill();
+}
+
+var counter = 0;
+
+setInterval(draw, 30)
+
+  function addData(tweet) {
+    data[data.length] = [(tweet.coords[1]+180)*10, (tweet.coords[0]+180)*10]
+  };
+
+socket.on('tweet', function(data){
+  var tweetObj = data.tweet;
+  //   stripPunctuation(tweetObj.text).split(' ').forEach(function(element){
+  //   if(sentimentLookup[element]){
+  //     tweetObj.sentiment += sentimentLookup[element];
+  //   }
+  // })
+  addData(tweetObj);
+  counter += 1;
+  $('#tweetCount').text(counter);
+  socket.emit('echo', data);
+})
+
+$('.stop-connection').on('click', function() {
+  socket.emit('stop');
+});
+socket.on('stop echo', function() {
+  console.log('Stream stopped.');
+});
+});
